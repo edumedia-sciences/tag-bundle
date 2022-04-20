6562029edf843b833d56e16df24a264503b39105
Quentin <quentin.t@gmail.com>
2022-04-20 10:51:26 +0200

    Add service alias Add Twig global

--- src/DependencyInjection/eduMediaTagExtension.php
+++ src/DependencyInjection/eduMediaTagExtension.php
@@ -15,9 +15,9 @@ class eduMediaTagExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-
         $loader = new YamlFileLoader($container, new FileLocator(\dirname(__DIR__).'/Resources/config'));
         $loader->load('services.yaml');
+        $loader->load('twig.yaml');
     }
 
     public function getAlias(): string
